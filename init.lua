89890569e7532fcf17692072c835cc762efbe7be
Gregory Donovan <gregory.m.donovan@gmail.com>
2024-11-21 21:36:21 -0500

    Added a vim opt path.

--- init.lua
+++ init.lua
@@ -158,6 +158,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Add project to the path
+vim.opt.path = '**'
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
